f415a1ef461f217cf5501b5ac6752d0c632f2972
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2023-05-26 19:56:07 -0400

    Update lisk-create-validator.sh

--- SH/lisk-create-validator.sh
+++ SH/lisk-create-validator.sh
@@ -14,18 +14,27 @@ read -rs ValidatorPassphrase
 echo "? Please enter password:  [hidden]"
 read -rs ValidatorPassword
 
-echo -e "Create config/keys.json"
+echo -e "2.1 Create the validator keys (config/keys.json)"
+
 lisk-core keys:create --output config/keys.json --passphrase "$ValidatorPassphrase" --password "$ValidatorPassword"
 
-echo -e "Parse config/keys.json"
+echo -e "2.2 Create the Register Validator transaction"
+
 LiskKeys_JsonData=$( cat config/keys.json )
 ValidatorGeneratorKey=$( echo "$LiskKeys_JsonData" | jq '.keys[0].plain.generatorKey' |  tr -d '"' )
 ValidatorBlsKey=$( echo "$LiskKeys_JsonData" | jq '.keys[0].plain.blsKey' |  tr -d '"' )
 ValidatorProofOfPossession=$( echo "$LiskKeys_JsonData" | jq '.keys[0].plain.blsProofOfPossession' |  tr -d '"' )
 JsonParams='{"name":"'"$ValidatorName"'","generatorKey":"'"$ValidatorGeneratorKey"'","blsKey":"'"$ValidatorBlsKey"'","proofOfPossession":"'"$ValidatorProofOfPossession"'"}'
-
-echo -e "Create Transaction"
 Transaction=$( lisk-core transaction:create pos registerValidator 1100000000 --params="$JsonParams" --passphrase "$ValidatorPassphrase" | jq '.transaction' |  tr -d '"' )
 
-echo -e "Send Transaction"
+echo -e "2.3 Send the transaction"
+
 lisk-core transaction:send "$Transaction"
+
+echo -e "3.0 Import the validator keys"
+
+lisk-core keys:import --file-path ./config/keys.json
+
+echo -e "3.1. Verifying correct import"
+
+lisk-core endpoint:invoke generator_getAllKeys --pretty
