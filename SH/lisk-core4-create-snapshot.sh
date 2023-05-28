a671009d3d90f569ef9a03e9f489a78f23e6ceb0
Gr33nDrag0n <Gr33nDrag0n69@users.noreply.github.com>
2023-05-28 15:20:25 -0400

    Update lisk-core4-create-snapshot.sh

--- SH/lisk-core4-create-snapshot.sh
+++ SH/lisk-core4-create-snapshot.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -e
 
-# Lisk-Core v4 Beta 0
+# Lisk-Core v4
 # Gr33nDrag0n v1.0 (2023-05-28)
 
+# Can be edited
 
 OUTPUT_DIRECTORY="$HOME/beta4-snapshot"
-OUTPUT_GZIP_FILENAME="blockchain.db.tar.gz"
+DAYS_TO_KEEP="3" # Use 0 to disable the feature
+
+# Do not edit
+
+OUTPUT_GZIP_FILENAME="blockchain.tar.gz"
 OUTPUT_GZIP_FILEPATH="$OUTPUT_DIRECTORY/$OUTPUT_GZIP_FILENAME"
 OUTPUT_HASH_FILEPATH="$OUTPUT_DIRECTORY/$OUTPUT_GZIP_FILENAME.SHA256"
-
-DAYS_TO_KEEP="3" # Use 0 to disable the feature
 
 ### Function(s) #######################################################################################################
 
@@ -42,7 +45,7 @@ lisk-core blockchain:export --output "$OUTPUT_DIRECTORY"
 echo -e "$(now) Create $OUTPUT_GZIP_FILENAME.SHA256"
 cd "$OUTPUT_DIRECTORY" && sha256sum "$OUTPUT_GZIP_FILENAME" > "$OUTPUT_HASH_FILEPATH"
 
-OUTPUT_GZIP_COPY_FILENAME="blockchain-$HEIGHT.db.tar.gz"
+OUTPUT_GZIP_COPY_FILENAME="blockchain-$HEIGHT.tar.gz"
 OUTPUT_GZIP_COPY_FILEPATH="$OUTPUT_DIRECTORY/$OUTPUT_GZIP_COPY_FILENAME"
 OUTPUT_HASH_COPY_FILEPATH="$OUTPUT_DIRECTORY/$OUTPUT_GZIP_COPY_FILENAME.SHA256"
 
@@ -61,7 +64,7 @@ chmod 644 "$OUTPUT_HASH_COPY_FILEPATH"
 if [ "$DAYS_TO_KEEP" -gt 0 ]; then
     echo -e "$(now) Deleting snapshots older then $DAYS_TO_KEEP day(s)"
     mkdir -p "$OUTPUT_DIRECTORY" &> /dev/null
-    find "$OUTPUT_DIRECTORY" -name "blockchain-*.db.tar.gz*" -mtime +"$(( DAYS_TO_KEEP - 1 ))" -exec rm {} \;
+    find "$OUTPUT_DIRECTORY" -name "blockchain-*.tar.gz*" -mtime +"$(( DAYS_TO_KEEP - 1 ))" -exec rm {} \;
 fi
 
 exit 0
